3c30ecd89ba0ad620fc0b835d3c9491ce0fed168
Matthias Beitl <matthias.beitl@beeone.at>
2015-02-02 14:32:01 +0100

    Simplified Gruntfile, Firefox SVG-as-dataURI issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,29 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
+		// clean staging directory
+		clean: {
+			build: ['<%= pkg.paths.stage %>']
+		},
+
+		// minify css
+		cssmin: {
+			main: {
+				files: {
+					'<%= pkg.paths.stage %>/main.min.css': ['static/normalize.css', 'static/style.css']
+				}
+			}
+		},
+
+		// uglify js
+		uglify: {
+			main: {
+				files: {
+					'<%= pkg.paths.stage %>/main.min.js': 'static/mailto.js'
+				}
+			}
+		},
+
 		// jade compile
 		jade: {
 			compile: {
@@ -35,76 +58,26 @@ module.exports = function (grunt) {
 					{src: ['LICENSE'], dest: '<%= pkg.paths.dist %>/'}
 				]
 			}
-		},
-
-		// clean staging directory
-		clean: {
-			build: ['<%= pkg.paths.stage %>'],
-		},
-
-		// concat files
-		concat: {
-			options: {
-				process: function (src, filepath) {
-					if (filepath.indexOf('.js') !== -1) {
-						src = src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, ''); // use strict statements
-						src = src + ';';
-					}
-					src = src.replace(/\/\*[^\*]*\*\//g, ''); // block comments
-					return src;
-				}
-			},
-			js: {
-				files: {
-					'<%= pkg.paths.stage %>/main.min.js': ['<%= pkg.paths.stage %>/mailto.min.js']
-				}
-			},
-			css: {
-				files: {
-					'<%= pkg.paths.stage %>/main.min.css': ['<%= pkg.paths.stage %>/normalize.min.css', '<%= pkg.paths.stage %>/style.min.css']
-				}
-			}
-		},
-
-		// uglify js
-		uglify: {
-			js: {
-				files: {
-					'<%= pkg.paths.stage %>/mailto.min.js': 'static/mailto.js'
-				}
-			}
-		},
-
-		// minify css
-		cssmin: {
-			minify: {
-				expand: true,
-				cwd: 'static/',
-				src: ['normalize.css', 'style.css'],
-				dest: '<%= pkg.paths.stage %>/',
-				ext: '.min.css'
-			}
 		}
 
 	});
 
 	// Load the plugins
 	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-jade');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
 	grunt.registerTask(
 		'build',
 		'Prepares project deployment (minification, concatenation)',
-		['clean:build', 'uglify:js', 'concat:js', 'cssmin:minify', 'concat:css']
+		['clean:build', 'uglify:main', 'cssmin:main']
 	);
 	grunt.registerTask(
 		'release',
 		'Deploys the project (copy assets and generate HTML)',
-		['clean:build', 'uglify:js', 'concat:js', 'cssmin:minify', 'concat:css', 'jade:compile', 'copy:assets']
+		['clean:build', 'uglify:main', 'cssmin:main', 'jade:compile', 'copy:assets']
 	);
 
 	// Default task(s).
